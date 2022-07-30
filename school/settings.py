1a7c03c057d42582d7b8a3374ceda13df26b689f
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-07-30 21:27:00 +0600

    add requirement

--- school/settings.py
+++ school/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from decouple import config
 from pathlib import Path
-
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Configure Django App for Heroku on settings.py.
+
+django_on_heroku.settings(locals())
